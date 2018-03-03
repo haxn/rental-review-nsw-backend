dd3784a7107f621fac5220e1ccbb1c701a97d16a
Stan Tsouvallas <stan_ts@hotmail.com>
2018-03-03 19:49:52 +1100

    finish auth workflow

--- schema.py
+++ schema.py
@@ -32,28 +32,88 @@ class createUser(graphene.Mutation):
 
     @staticmethod
     def mutate(root, info, **args):
-        user_info = {
-            'name': args.get('name'),
-            'email': args.get('email'),
-            'fbId': args.get('fbId')
-        }
+        try:
+            user_info = {
+                'name': args.get('name'),
+                'email': args.get('email'),
+                'fbId': args.get('fbId')
+            }
+            user = UserModel(**user_info)
 
-        user = UserModel(**user_info)
 
-        db_session.add(user)
-        db_session.commit()
+            ## check for existing user
+            query = Users.get_query(info)
+            existing_user = query.filter(UserModel.fbId == user_info['fbId']).first()
 
-        ok = True
+            if existing_user is None:
+                db_session.add(user)
+            else:
+                existing_user.name = user_info['name']
+                existing_user.email = user_info['email']
 
-        encoded_jwt = jwt.encode(
-            {
-                **user_info,
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=90)
-            },
-            SECRET_SALT, algorithm='HS256'
-        )
+                db_session.add(existing_user)
+
+            db_session.commit()
+
+
+
+            ok = True
+            encoded_jwt = jwt.encode(
+                {
+                    **user_info,
+                    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=90)
+                },
+                SECRET_SALT, algorithm='HS256'
+            ).decode('utf-8')
+
+        except Exception as e:
+            print(e)
+            ok = False
+            user = None
+            encoded_jwt = None
+            db_session.rollback()
 
         return createUser(user=user, ok=ok, encodedJwt=encoded_jwt)
+
+
+class getAuthToken(graphene.Mutation):
+    class Arguments:
+        fbId = graphene.String()
+
+    ok = graphene.Boolean()
+    encodedJwt = graphene.String()
+
+    @staticmethod
+    def mutate(root, info, **args):
+        try:
+            fb_id = args.get('fbId')
+            query = Users.get_query(info)
+            user = query.filter(UserModel.fbId == fb_id).first()
+            print(dir(user))
+            user_info = {
+                'name': user.name,
+                'email': user.email,
+                'fbId': fb_id
+            }
+
+            ok = True
+            encoded_jwt = jwt.encode(
+                {
+                    **user_info,
+                    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=90)
+                },
+                SECRET_SALT, algorithm='HS256'
+            ).decode('utf-8')
+
+        except Exception as e:
+            print(str(e))
+            ok = False
+            encoded_jwt = None
+        print(encoded_jwt)
+        return getAuthToken(ok=ok, encodedJwt=encoded_jwt)
+
+
+
 
 # # Used to Change Username with Email
 # class changeUsername(graphene.Mutation):
@@ -78,22 +138,43 @@ class createUser(graphene.Mutation):
 
 class refreshAuthToken(graphene.Mutation):
     class Arguments:
-        auth_token = graphene.String()
+        encodedJwt = graphene.String()
 
     ok = graphene.Boolean()
-    user = graphene.Field(Users)
+    encodedJwt = graphene.String()
 
     @staticmethod
     def mutate(root, info, **args):
-        query = Users.get_query(context)
-        auth_token = args.get('auth_token')
-        # username = args.get('username')
-        # user = query.filter(UserModel.email == email).first()
-        # user.username = username
-        db_session.commit()
-        ok = True
+        try:
+            encoded_jwt = args.get('encodedJwt')
+            user_info = jwt.decode(encoded_jwt, SECRET_SALT, algorithm='HS256')
+            new_encoded_jwt = jwt.encode(
+                {
+                    **user_info,
+                    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=90)
+                },
+                SECRET_SALT, algorithm='HS256'
+            )
 
-        return refreshAuthToken(user=user, ok = ok)
+            ok = True
+
+        except Exception as e:
+            print(str(e))
+            ok = False
+            new_encoded_jwt = None
+
+
+        return refreshAuthToken(ok=ok, encodedJwt=new_encoded_jwt)
+
+        # query = Users.get_query(context)
+        # auth_token = args.get('auth_token')
+        # # username = args.get('username')
+        # # user = query.filter(UserModel.email == email).first()
+        # # user.username = username
+        # db_session.commit()
+        # ok = True
+        #
+        # return refreshAuthToken(user=user, ok = ok)
 
 
 class Query(graphene.ObjectType):
@@ -111,7 +192,8 @@ class Query(graphene.ObjectType):
 
 class MyMutations(graphene.ObjectType):
     create_user = createUser.Field()
-    # change_username = changeUsername.Field()
+    refresh_auth_token = refreshAuthToken.Field()
+    get_auth_token = getAuthToken.Field()
 
 schema = graphene.Schema(query=Query, mutation=MyMutations, types=[Users])
 
